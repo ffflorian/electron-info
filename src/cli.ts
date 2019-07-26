094c4e93b24c2eff71858cb9f6468b0a5a2294c3
Florian Keller <ffflorian@users.noreply.github.com>
2019-07-26 11:34:27 +0200

    feat: Parse dist tags (#9)

--- src/cli.ts
+++ src/cli.ts
@@ -11,7 +11,9 @@ const needsN = (name: string) => `a${/^[aeoui]/.test(name) ? 'n' : ''} ${name}`;
 
 program
   .name(name)
-  .description(description)
+  .description(
+    `${description}\n\nAllowed version argument inputs:\n  - SemVer versions (e.g. "~7")\n  - npm dist tags (e.g. "5-0-x", only Electron)\n  - "all"`
+  )
   .option('-f, --force', 'Force downloading the latest release file')
   .option('-r, --raw', 'Output raw JSON')
   .option('--no-colors', `Don't use colors for displaying`)
@@ -21,7 +23,7 @@ program
 program
   .command('electron')
   .alias('e')
-  .description('Get informations about an Electron version (e.g. "electron 5.0.7" or "electron latest")')
+  .description('Get informations about an Electron release')
   .arguments('[version]')
   .action(async (version, {parent}) => {
     if (!version) {
@@ -44,7 +46,7 @@ for (const dependency in SupportedDependencies) {
   program
     .command(dependency)
     .alias(dependency[0])
-    .description(`Get informations about ${needsN(dependency)} version`)
+    .description(`Get informations about ${needsN(dependency)} release`)
     .arguments('[version]')
     .action(async (version, {parent}) => {
       if (!version) {
@@ -67,7 +69,7 @@ for (const dependency in SupportedDependencies) {
 
 program
   .command('all')
-  .description('Get informations about all versions')
+  .description('Get informations about all releases')
   .action(async ({parent}) => {
     try {
       const releases = await new ElectronInfo({electronPrereleases: parent.prereleases}).getAllReleases(
